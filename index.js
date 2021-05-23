198d1f65e6ebcccab91c1dfca16b4d3a43ccb712
Jhon Alexander Gil Beltran <johnalexand3rg@gmail.com>
2021-05-22 21:12:18 -0500

    modifique la estructura

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-firebase.auth().createUserWithEmailAndPassword('johnalexand3rg@gmail.com', 'asd123qwe456')
-.then((algo)=> console.log(algo))
-.catch((error) => {
+let username = 'johnalexand3rg@gmail.com';
+let password = 'asd123qwe456';
+
+firebase.auth().createUserWithEmailAndPassword(username, password)
+.then((succes) => {
+    console.log(succes)
+})
+.catch((error) =>  {
     if (error.code == "auth/email-already-in-use") {
         alert("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
     } else {
-        alert(error.message);
+        console.log(error);
     }
 });
